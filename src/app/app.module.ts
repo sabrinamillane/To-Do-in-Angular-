1862b5318e5ba109f0e034fdc4eab5211e1cb273
Sabrina Silva <Sabrina.Silva@fit-tecnologia.org.br>
2023-03-20 11:42:34 -0400

    projeto To-Do em Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Module Home
+import { HomeModule } from './modules/home/home.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
